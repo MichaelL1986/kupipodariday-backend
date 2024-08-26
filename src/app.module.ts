3600867d7b6089f3edf2ebf63919704456b5ce8d
MichaelL1986 <167879507+MichaelL1986@users.noreply.github.com>
2024-08-26 13:46:54 +0300

    env config added

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,14 @@ import { WishesModule } from './wishes/wishes.module';
 import { WishlistsModule } from './wishlists/wishlists.module';
 import { OffersModule } from './offers/offers.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseConfig } from './config/DatabaseConfig';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'student',
-      password: 'student',
-      database: 'nest_project',
-      // entities: [User, Offer, Wish, Wishlist],
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConfig,
     }),
     UsersModule,
     WishesModule,
---
+++ src/config/DatabaseConfig.ts
@@ -0,0 +1,19 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+export class DatabaseConfig implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: this.configService.get<string>('DB_HOST'),
+      username: this.configService.get<string>('DB_USER'),
+      password: this.configService.get<string>('DB_PASSWORD'),
+      port: this.configService.get<number>('DB_PORT'),
+      database: this.configService.get<string>('DB_NAME'),
+      autoLoadEntities: true,
+      synchronize: true,
+    };
+  }
+}
